777061a4277d80c112e5faee2cff291d795df837
Salman Javed <simply.ingenious@gmail.com>
2023-08-03 11:38:39 +0500

    neovide config

--- lua/simplyingenious/init.lua
+++ lua/simplyingenious/init.lua
@@ -16,3 +16,22 @@ autocmd('TextYankPost', {
     })
   end,
 })
+
+if vim.g.neovide then
+  vim.o.guifont = "ellograph_cf:h14"
+
+  vim.g.neovide_padding_top = 8
+  vim.g.neovide_padding_bottom = 8
+  vim.g.neovide_padding_right = 8
+  vim.g.neovide_padding_left = 8
+
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_cursor_vfx_particle_density = 8.0
+
+  vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
+  vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
