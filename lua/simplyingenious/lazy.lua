32b0fc1ad03888042ceb383334492927801ba6a5
Salman Javed <simply.ingenious@gmail.com>
2023-12-26 13:24:50 +0500

    plugins removed for minimal setup

--- lua/simplyingenious/lazy.lua
+++ lua/simplyingenious/lazy.lua
@@ -26,9 +26,6 @@ require("lazy").setup({
       vim.keymap.set('n', '<leader>psl', function()
         builtin.live_grep()
       end)
-      -- vim.keymap.set('n', '<leader>ps', function()
-      --   builtin.grep_string()
-      -- end)
       vim.keymap.set('n', '<leader>ps', function()
         builtin.grep_string({ search = vim.fn.input("Grep ❯ ") })
       end)
@@ -46,10 +43,10 @@ require("lazy").setup({
 
       configs.setup({
         -- A list of parser names, or "all" (the four listed parsers should always be installed)
-        ensure_installed = { "javascript", "typescript", "css", "lua", "vim" },
+        -- ensure_installed = { "javascript", "typescript", "css", "lua", "vim" },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
+        -- sync_install = false,
 
         -- Automatically install missing parsers when entering buffer
         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
@@ -77,32 +74,7 @@ require("lazy").setup({
     end
   },
 
-  { 'tpope/vim-fugitive' },
-
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    dependencies = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },             -- Required
-      { 'williamboman/mason.nvim' },           -- Optional
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
-      -- Snippets
-      { 'L3MON4D3/LuaSnip' },             -- Required
-      { 'rafamadriz/friendly-snippets' }, -- Optional
-
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },         -- Required
-      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-      { 'hrsh7th/cmp-buffer' },       -- Optional
-      { 'hrsh7th/cmp-path' },         -- Optional
-      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
-
-      -- Additional Plugins
-    }
-  },
+  -- { 'tpope/vim-fugitive' },
 
   {
     'JoosepAlviste/nvim-ts-context-commentstring',
@@ -122,13 +94,4 @@ require("lazy").setup({
     end,
     lazy = false
   },
-  { 'lewis6991/gitsigns.nvim',      opts = {} },
-  {
-    'norcalli/nvim-colorizer.lua',
-    event = { "BufRead", "BufNewFile" },
-    config = function()
-      require 'colorizer'.setup()
-    end
-  },
-  -- { 'RRethy/vim-illuminate', opts = {} },
 })
