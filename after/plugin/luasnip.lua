f477d747aaca8858f8b2671a366e263c9be38670
Salman Javed <simply.ingenious@gmail.com>
2024-01-04 12:02:36 +0500

    added ci snippet for javascript

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,16 +1,38 @@
 local ls = require('luasnip')
+-- some shorthands...
+local snip = ls.snippet
+local node = ls.snippet_node
+local text = ls.text_node
+local insert = ls.insert_node
+local func = ls.function_node
+local choice = ls.choice_node
+local dynamicn = ls.dynamic_node
+
 vim.keymap.set({ "i", "s" }, "<C-x>", function()
   ls.expand_or_jump()
-end, {silent = true})
+end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-n>", function()
   ls.change_choice(1)
-end, {silent = true})
+end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-p>", function()
   ls.change_choice(-1)
-end, {silent = true})
+end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-l>", function()
   ls.jump(1)
-end, {silent = true})
+end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-j>", function()
   ls.jump(-1)
-end, {silent = true})
+end, { silent = true })
+
+
+ls.add_snippets(nil, {
+  javascript = {
+    snip({
+      trig = "ci",
+      namr = "Console Info",
+      dscr = "console.info",
+    }, {
+      text("console.info("), insert(1), text(")")
+    })
+  }
+})
