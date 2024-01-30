f26cc732b6b85ab66274ecf7d334be504c0dae01
Salman Javed <simply.ingenious@gmail.com>
2024-01-30 11:43:11 +0500

    Added plugins added fzf-native, todo-comments plugins.

--- lua/simplyingenious/lazy.lua
+++ lua/simplyingenious/lazy.lua
@@ -9,6 +9,7 @@ require("lazy").setup({
     end,
   },
 
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.2',
@@ -54,11 +55,20 @@ require("lazy").setup({
               }
             }
           }
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,                   -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          }
         }
       }
+      require('telescope').load_extension('fzf')
     end
   },
-
   {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
@@ -225,6 +235,11 @@ require("lazy").setup({
     config = function()
       require 'colorizer'.setup()
     end
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {}
   },
   -- { 'RRethy/vim-illuminate', opts = {} },
 })
